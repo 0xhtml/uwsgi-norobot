67b662cff6fbfb5520d4f89537149f179b5a84aa
0xhtml <34682885+0xhtml@users.noreply.github.com>
2022-10-21 00:27:46 +0200

    Refactor

--- norobot.c
+++ norobot.c
@@ -1,6 +1,7 @@
-#include <uwsgi.h>
-#include <uuid/uuid.h>
 #include <openssl/sha.h>
+#include <stdbool.h>
+#include <uuid/uuid.h>
+#include <uwsgi.h>
 
 static char *get_cookie(struct wsgi_request *request, char *key, uint16_t *len) {
     return uwsgi_get_cookie(request, key, strlen(key), len);
@@ -10,11 +11,11 @@ static char *get_var(struct wsgi_request *request, char *key, uint16_t *len) {
     return uwsgi_get_var(request, key, strlen(key), len);
 }
 
-static void get_ip(unsigned char *out, struct wsgi_request *request) {
+static void get_ip(struct wsgi_request *request, unsigned char *out) {
     uint16_t len;
     char *ip = get_var(request, "REMOTE_ADDR", &len);
-
     int j = 0;
+
     for (int i = 0; i < 4; ++i) {
         out[i] = 0;
 
@@ -26,17 +27,37 @@ static void get_ip(unsigned char *out, struct wsgi_request *request) {
     }
 }
 
-static void gen_secret(struct wsgi_request *request, struct uwsgi_route *route, unsigned char *out) {
-    unsigned char data[4 + sizeof(uuid_t)] = {};
-    get_ip(data, request);
-    memcpy(data + 4, route->data, sizeof(uuid_t));
-    SHA256(data, sizeof(data), out);
+#define RAW_HASH_LEN SHA256_DIGEST_LENGTH
+#define HASH_LEN (RAW_HASH_LEN * 2 + 1)
+
+static void gen_hash(unsigned char *in, uint16_t in_len, unsigned char *raw_out, char *out) {
+    SHA256(in, in_len, raw_out);
+    for (uint16_t i = 0; i < RAW_HASH_LEN; ++i) {
+        sprintf(out + i * 2, "%02x", raw_out[i]);
+    }
 }
 
-static void to_str(unsigned char *in, uint16_t len, char *out) {
-    for (uint16_t i = 0; i < len; ++i) {
-        sprintf(out + i * 2, "%02x", in[i]);
-    }
+static void gen_shared_secret(struct wsgi_request *request, struct uwsgi_route *route, unsigned char *raw_out, char *out) {
+    struct {
+        uuid_t uuid;
+        unsigned char ip[4];
+    } data;
+
+    memcpy(data.uuid, route->data, sizeof(uuid_t));
+    get_ip(request, data.ip);
+
+    gen_hash((unsigned char *) &data, sizeof(data), raw_out, out);
+}
+
+static bool check_cookie(struct wsgi_request *request, char *shared_secret) {
+    uint16_t cookie_len;
+    char *cookie = get_cookie(request, "secret", &cookie_len);
+
+    if (cookie == NULL) return false;
+    if (cookie_len != HASH_LEN - 1) return false; // w/o terminating null-byte
+    if (memcmp(cookie, shared_secret, cookie_len)) return false;
+
+    return true;
 }
 
 static char *status = "200 OK";
@@ -45,27 +66,20 @@ static char *content_type = "text/html; charset=utf-8";
 #define ENTER_DIGITS 3
 
 static int norobot_router_func(struct wsgi_request *request, struct uwsgi_route *route) {
-    unsigned char secret[SHA256_DIGEST_LENGTH];
-    gen_secret(request, route, secret);
+    unsigned char raw_shared_secret[RAW_HASH_LEN];
+    char shared_secret[HASH_LEN];
+    gen_shared_secret(request, route, raw_shared_secret, shared_secret);
 
-    char secret_str[sizeof(secret) * 2 + 1] = {};
-    to_str(secret, sizeof(secret), secret_str);
-
-    uint16_t cookie_len;
-    char *cookie = get_cookie(request, "secret", &cookie_len);
-
-    if (cookie != NULL && cookie_len == sizeof(secret) * 2 && !memcmp(cookie, secret_str, cookie_len)) {
+    if (check_cookie(request, shared_secret)) {
         return UWSGI_ROUTE_NEXT;
     }
 
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256(secret, sizeof(secret), hash);
-
-    char hash_str[sizeof(hash) * 2 + 1] = {};
-    to_str(hash, sizeof(hash), hash_str);
+    unsigned char raw_hash[RAW_HASH_LEN];
+    char hash[HASH_LEN];
+    gen_hash(raw_shared_secret, sizeof(raw_shared_secret), raw_hash, hash);
 
     char content[2048] = {};
-    int offset = (int) sizeof(secret) * 2 - CALC_DIGITS - ENTER_DIGITS;
+    int offset = HASH_LEN - 1 - CALC_DIGITS - ENTER_DIGITS;
     uint16_t content_len = snprintf(
         content,
         sizeof(content),
@@ -112,11 +126,11 @@ static int norobot_router_func(struct wsgi_request *request, struct uwsgi_route 
         "    </script>\n"
         "</body>\n"
         "</html>",
-        ENTER_DIGITS, secret_str + offset,
+        ENTER_DIGITS, shared_secret + offset,
         ENTER_DIGITS,
-        offset, secret_str,
+        offset, shared_secret,
         CALC_DIGITS, 0,
-        hash_str,
+        hash,
         CALC_DIGITS
     );
 
@@ -137,18 +151,20 @@ static int norobot_router_func(struct wsgi_request *request, struct uwsgi_route 
 }
 
 static int norobot_router(struct uwsgi_route *route, char *args) {
-    route->func = norobot_router_func;
+    if (strlen(args) == 0) {
+        char uuid[37];
+        uwsgi_uuid(uuid);
+        uwsgi_log("invalid route syntax: norobot missing uuid, replace with norobot:%s\n", uuid);
+        exit(1);
+    }
 
     route->data = uwsgi_malloc(sizeof(uuid_t));
-
-    if (strlen(args) > 0) {
-        if (uuid_parse(args, route->data)) {
-            uwsgi_log("error parsing uuid %s\n", args);
-            exit(1);
-        }
-    } else {
-        uuid_generate(route->data);
+    if (uuid_parse(args, route->data)) {
+        uwsgi_log("invalid route syntax: error parsing uuid %s\n", args);
+        exit(1);
     }
+
+    route->func = norobot_router_func;
 
     return 0;
 }
